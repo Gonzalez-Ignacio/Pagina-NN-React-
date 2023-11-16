0eb807e6f4c69417e5c843c3b38c244d33570d83
Nahuel <salvatierra.nahuel@gmai.com>
2023-11-16 18:47:17 -0300

    Update functionality buttons component

--- src/componentes/Cards.jsx
+++ src/componentes/Cards.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import Boton from './Boton';
 
-function Cards({ img, alt, titulo, descripcion }) {
+function Cards({ img, alt, titulo, descripcion, link}) {
+
+    const handleOnClik = ()=>{
+        window.open(link, '_blank')
+    }
+
     return (
         <div className="col-lg-4 mb-5">
             <div className="card h-100 shadow border-0">
@@ -13,16 +18,14 @@ function Cards({ img, alt, titulo, descripcion }) {
                     {/* <div className="badge bg-primary bg-gradient rounded-pill mb-2"> <i
                         className="bi bi-heart-fill text-danger"></i> +86                                      Boton contador de Like (Falta Cambiarlo a Boton)
                     </div> */}
-                    <a className="text-decoration-none link-dark stretched-link" href="#!">
                         <h5 className="card-title mb-3"> {titulo} </h5>
-                    </a>
                     <p className="card-text mb-0"> {descripcion} </p>
                 </div>
                 <div className="card-footer p-4 pt-0 bg-transparent border-top-0">
                     <div className="d-flex align-items-end justify-content-between">
                         <div className="d-flex align-items-center">
                             <div className="small">
-                                <Boton nombreBoton={"Obtener"} />  {/* Falta agregar onClick  */}
+                                <Boton nombreBoton={"Obtener"} onClick={handleOnClik}/>  {/* Falta agregar onClick  */}
                             </div>
                         </div>
                     </div>
