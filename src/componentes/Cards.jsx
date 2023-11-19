27969c57b8e463d3d1223600beb027e3b865d3c0
Nahuel <salvatierra.nahuel@gmai.com>
2023-11-19 17:02:35 -0300

    Update texts and buttons functionality

--- src/componentes/Cards.jsx
+++ src/componentes/Cards.jsx
@@ -1,38 +1,36 @@
-import React from 'react';
-import Boton from './Boton';
+import React from "react";
+import Boton from "./Boton";
 
-function Cards({ img, alt, titulo, descripcion, link}) {
+function Cards({ img, alt, titulo, descripcion, link }) {
+	const handleOnClik = () => {
+		window.open(link, "_blank");
+	};
 
-    const handleOnClik = ()=>{
-        window.open(link, '_blank')
-    }
-
-    return (
-        <div className="col-lg-4 mb-5">
-            <div className="card h-100 shadow border-0">
-                <img className="card-img-top"
-                    src={img}
-                    alt={alt}
-                />
-                <div className="card-body p-4">
-                    {/* <div className="badge bg-primary bg-gradient rounded-pill mb-2"> <i
+	return (
+		<div className="col-lg-4 mb-5">
+			<div className="card h-100 shadow border-0">
+				<img className="card-img-top" src={img} alt={alt} />
+				<div className="card-body p-4">
+					{/* <div className="badge bg-primary bg-gradient rounded-pill mb-2"> <i
                         className="bi bi-heart-fill text-danger"></i> +86                                      Boton contador de Like (Falta Cambiarlo a Boton)
                     </div> */}
-                        <h5 className="card-title mb-3"> {titulo} </h5>
-                    <p className="card-text mb-0"> {descripcion} </p>
-                </div>
-                <div className="card-footer p-4 pt-0 bg-transparent border-top-0">
-                    <div className="d-flex align-items-end justify-content-between">
-                        <div className="d-flex align-items-center">
-                            <div className="small">
-                                <Boton nombreBoton={"Obtener"} onClick={handleOnClik}/>  {/* Falta agregar onClick  */}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+					<h5 className="card-title mb-3"> {titulo} </h5>
+					<p className="card-text mb-0"> {descripcion} </p>
+				</div>
+				<div className="card-footer p-4 pt-0 bg-transparent border-top-0">
+					<div className="d-flex align-items-end justify-content-between">
+						<Boton
+							nombreBoton={"Obtener"}
+							onClick={handleOnClik}
+                            className="flex-fill "
+						/>{" "}
+						{/* Falta agregar onClick  */}
+						<Boton nombreBoton="Visitar Sitio" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Cards;
--- src/paginas/precio/Precio.jsx
+++ src/paginas/precio/Precio.jsx
@@ -5,7 +5,6 @@ function Precio() {
     const cardsData = [                                                                   //Creamos un Objeto donde Objeto representara una Card 
         {
             titulo: 'Landing Page',
-            precio: '70.000',
             caracteristicas: [
                 { classNameIcono: 'bi bi-check text-primary', descripcion: 'Diseño Personalizado', destacada: false },          //Damos Icono, la descripcion y destacada boleano para la clase de "text mute" en caso de ser verdadero
                 { classNameIcono: 'bi bi-check text-primary', descripcion: 'Formularios de Captura', destacada: false},        //En caso de ser Falso, la clase queda vacía
@@ -26,7 +25,6 @@ function Precio() {
         },
         {
             titulo: 'Enterprise',
-            precio: '90.000',
             caracteristicas: [
                 { classNameIcono: 'bi bi-check text-primary', descripcion: 'Diseño Personalizado', destacada: false },          
                 { classNameIcono: 'bi bi-check text-primary', descripcion: 'Formularios de Captura', destacada: false},      
@@ -49,7 +47,6 @@ function Precio() {
         },
         {
             titulo: 'E-commerce',
-            precio: '110.000',
             caracteristicas: [
                 { classNameIcono: 'bi bi-check text-primary', descripcion: 'Diseño Personalizado', destacada: false },          
                 { classNameIcono: 'bi bi-check text-primary', descripcion: 'Formularios de Captura', destacada: false},      
@@ -74,7 +71,7 @@ function Precio() {
     return (
         <div>
             <section className='bg-light py-5'>
-                <div className='container px-5 my-5'>
+                <div className='container-flex px-5 my-5'>
                     <div className='text-center mb-5'>
                         <h1 className='fw-bolder'>Paga a medida que creces</h1>
                         <p className='lead fw-normal text-muted mb-0'>
@@ -83,7 +80,7 @@ function Precio() {
                     </div>
                     <div className='row gx-5 justify-content-center'>
                         {cardsData.map((card, index) => (                                //Indicamos que itere en cada una de los objetos.
-                            <div key={index} className='col-lg-6 col-xl-4'>              {/*Le damos la Key para que optimice la actualización y rendimiento*/}
+                            <div key={index} className='col-lg-7 col-xl-4'>              {/*Le damos la Key para que optimice la actualización y rendimiento*/}
                                 <CardPrecio                                    //Entonces al tener un .map le estamos diciendo que por cada obj, cree una Card
                                     titulo={card.titulo}
                                     precio={card.precio}
